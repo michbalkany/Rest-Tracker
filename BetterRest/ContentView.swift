b2bfb1523ec5bfe06183db4cba5917bc391a237b
mich balkany <askmitchb@gmail.com>
2023-09-26 14:30:23 -0400

    hour and minute can take an Int, alert was added to show message once calculations for bedtime is done

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingMessage = false
+    
     var body: some View {
         NavigationView{
             VStack{
@@ -36,6 +40,11 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedTime)
             }
+            .alert(alertTitle, isPresented: $showingMessage){
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
             }
         }
     func calculateBedTime() {
@@ -43,10 +52,20 @@ struct ContentView: View {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
             
-            // more code here
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
-            // Something went wrong!
+            alertTitle = "Error"
+            alertMessage = "sorry there was a problem calculating your bedtime"
         }
+        showingMessage = true
     }
     }
 
