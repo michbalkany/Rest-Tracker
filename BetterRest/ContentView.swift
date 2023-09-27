63319e25334f05343e2c0178a47aab4d157f70f7
mich balkany <askmitchb@gmail.com>
2023-09-27 12:13:34 -0400

    removed the calculate button, and the alert message, title, and message box. replaced the number of cups stepper with a picker, changes the vstack sections to a sections for more visually appeasing design. the sleep results are now given instantly at the bottom of the screen in bigger and bolder letters

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,44 +10,18 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingMessage = false
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     
-    var body: some View {
-        NavigationView{
-            VStack{
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
-                }
-            .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedTime)
-            }
-            .alert(alertTitle, isPresented: $showingMessage){
-                Button("OK") { }
-            } message: {
-                Text(alertMessage)
-            }
-            }
-        }
-    func calculateBedTime() {
+    var sleepResults: String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -59,18 +33,43 @@ struct ContentView: View {
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             
             let sleepTime = wakeUp - prediction.actualSleep
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            return "Your ideal bedtime is" + sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
-            alertTitle = "Error"
-            alertMessage = "sorry there was a problem calculating your bedtime"
+            return "there was an error"
         }
-        showingMessage = true
     }
+    
+    
+    var body: some View {
+        NavigationView{
+            Form {
+                Section("When do you want to wake up?") {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                Section("Desired amount of sleep") {
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                Section("Daily coffee intake") {
+                    Picker("Number of cups", selection: $coffeeAmount){
+                        ForEach (1..<21){
+                            Text(String($0))
+                        }
+                    }
+                }
+                Text(sleepResults)
+                    .font(.title3)
+            }
+            .navigationTitle("BetterRest")
+        }
     }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        }
     }
-}
+
